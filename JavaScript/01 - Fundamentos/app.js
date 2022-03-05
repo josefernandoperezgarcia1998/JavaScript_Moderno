79073a83c2277479539d9759c52bbc798da9f6ef
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-03-04 22:41:56 -0600

    Notas de variables, comentarios y consola

---
+++ JavaScript/01 - Fundamentos/app.js
@@ -0,0 +1,17 @@
+// console.log('caca');
+
+//Esto es un comentario (El comentario se manifiesta por "//")
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+console.log(saludo);
+
+console.log(x);
+console.log({x});
+console.table({a,b,c});
