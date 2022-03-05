76af86d9bfd0494ad0f8ecb443ba65700988398e
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-03-05 12:23:21 -0600

    Creando la carpeta de assets y creando el archivo de alerts, donde se ve confirm(), prompt() y alert()

---
+++ JavaScript/01 - Fundamentos/assets/js/app.js
@@ -0,0 +1,21 @@
+// console.log('caca');
+alert('caca desde app.js');
+//Esto es un comentario (El comentario se manifiesta por "//")
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log(saludo);
+
+// console.log(x);
+// console.log({x});
+// console.table({a,b,c});
+
+c = "Hola de nuevo"
+
+var miNombre = 'Fernando';
