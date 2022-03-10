044b47faa061eaa3ec3d75e53a2e094b45d7255e
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-03-09 22:14:40 -0600

    Pro tip: Asignación de lógica booleana

--- JavaScript/01 - Fundamentos/assets/js/logica-booleana.js
+++ JavaScript/01 - Fundamentos/assets/js/logica-booleana.js
@@ -39,3 +39,31 @@ console.log(false || false);
 console.log(regresaTrue() || regresaFalse());
 
 console.log('4 condiciones ||: ', true || true || true || false); //true ó 1
+
+
+
+
+
+
+
+// Pro tip: Asignación con operadores
+
+console.log('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+
+const a1 = false && 'Hola mundo' && 150;
+const a2 = 'Hola' && 'Mundo' && soyFalso && true;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || soyNull || 'Yano soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Yano soy falso de nuevo' || true;
+
+console.log({a1, a2, a3, a4, a5});
+if ( regresaFalse() && regresaTrue && (true || false || true) ){
+    console.log('Hacr algo');
+} else {
+    console.log('Hacer otra cosa');
+}
