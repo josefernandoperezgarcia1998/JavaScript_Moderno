3a67d175646101560b1518d6ff5e2bbcf62c24f6
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-03-14 19:04:27 -0600

    Métodos en las clases

--- JavaScript/01 - Fundamentos/assets/js/classes/class.js
+++ JavaScript/01 - Fundamentos/assets/js/classes/class.js
@@ -11,11 +11,26 @@ class Persona {
         this.frase = frase;
     }
 
+    quienSoy() {
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}, bye.`);
+    }
+
+    miFrase(){
+        this.quienSoy();
+        console.log(`Frase: ${this.frase}`);
+    }
+
 }
 
 // Se crean objetos haciendo instancia de la clase Padre
 const spiderman = new Persona('Peter', 'Spider', 'No digas mmds Mari Jane');
 const ironman = new Persona('Tony', 'iron', 'Millonario, modelo y actor');
 
-console.log(spiderman);
-console.log(ironman);
+// console.log(spiderman);
+// console.log(ironman);
+
+// spiderman.quienSoy();
+// ironman.quienSoy();
+
+spiderman.miFrase();
+ironman.miFrase();
